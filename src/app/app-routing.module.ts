69639c1af67ef3bc79d43bb1742e2bab76aeca94
Jordi Mas <xordimas@gmail.com>
2023-04-12 22:50:28 +0200

    no message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'jordimas',
+        redirectTo: 'holacomva'
+    },
+    {
+        path: 'holacomva',
+        component: AppComponent
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
