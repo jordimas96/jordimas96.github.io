afbcbf081a6d7894124ba407217dfbf780da5dc9
Jordi Mas <xordimas@gmail.com>
2023-04-29 13:50:19 +0200

    (from git)

--- src/app/components/imatge-scroll/imatge-scroll.component.ts
+++ src/app/components/imatge-scroll/imatge-scroll.component.ts
@@ -12,10 +12,12 @@ export class ImatgeScrollComponent {
     @Input() src: string;
     public transY;
 
+    constructor(public m: MainService) { }
+
     @HostListener('window:scroll', ['$event'])
     onScroll() {
-        this.transY = -((window.pageYOffset / 200) * 12 - 6);
-        this.transY = Utils.numberInRange(this.transY, -6, 6);
+        this.transY = -((window.pageYOffset / (this.m.esMobil() ? 100 : 200)) * 13 - 6.5);
+        this.transY = Utils.numberInRange(this.transY, -6.5, 6.5);
     }
   
 }
