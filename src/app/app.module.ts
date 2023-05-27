c8b96addb022808664ebabc6458c7d59aa370dcc
Jordi Mas <xordimas@gmail.com>
2023-05-27 22:01:00 +0200

    (from git)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { TargetBlankDirective } from './directives/target-blank.directive';
 import { SkillComponent } from './components/skill/skill.component';
 import { GoogleAnalyticsModule } from './google-analytics/google-analytics.module';
 import { QRCodeModule } from 'angularx-qrcode';
+import { NavegacioTabComponent } from './components/navegacio-tab/navegacio-tab.component';
 
 @NgModule({
     declarations: [
@@ -31,7 +32,8 @@ import { QRCodeModule } from 'angularx-qrcode';
 
         // Directives //
         TargetBlankDirective,
-         SkillComponent,
+        SkillComponent,
+        NavegacioTabComponent,
     ],
     imports: [
         BrowserModule,
---
+++ src/app/components/navegacio-tab/navegacio-tab.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { MainService } from 'src/app/services/main.service';
+
+@Component({
+    selector: 'app-navegacio-tab',
+    templateUrl: './navegacio-tab.component.html',
+    styleUrls: ['./navegacio-tab.component.scss']
+})
+export class NavegacioTabComponent {
+
+
+    constructor(public m: MainService) {
+
+    }
+    ngOnInit() {
+
+    }
+
+
+
+
+
+
+
+    
+}
