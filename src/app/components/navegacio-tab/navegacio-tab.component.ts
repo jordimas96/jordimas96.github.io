cec252846f1debe4b8007fc34697c7fc1bd7cb86
Jordi Mas <xordimas@gmail.com>
2023-05-28 03:14:14 +0200

    (from git)

--- src/app/components/navegacio-tab/navegacio-tab.component.ts
+++ src/app/components/navegacio-tab/navegacio-tab.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { interval } from 'rxjs';
 import { MainService } from 'src/app/services/main.service';
 
 @Component({
@@ -8,19 +9,64 @@ import { MainService } from 'src/app/services/main.service';
 })
 export class NavegacioTabComponent {
 
+    @ViewChild("pill") pill: ElementRef;
 
-    constructor(public m: MainService) {
+    botoSeleccionat;
+
+    public rect: any;
+
+    movent = false;
+
+    constructor(public m: MainService, private elementRef: ElementRef) {
 
     }
     ngOnInit() {
+        this.m.afterRootFadeIn(this.afterRootFadeIn.bind(this));
 
     }
 
+    afterRootFadeIn() {
+        this.actPosPill(false);
 
+        interval(50).subscribe(() => {
+            if (!this.movent)
+                this.actPosPill(false);
+        });
+    }
 
+    onClick(target) {
+        this.botoSeleccionat = target;
+        this.m.u.scroll(0);
 
+        this.actPosPill(true);
+    }
 
+    actPosPill(animacio) {
+        if (!this.botoSeleccionat) this.botoSeleccionat = $("button.selected")[0];
+        let rect = this.botoSeleccionat.getBoundingClientRect();
+        let rectComp = this.elementRef.nativeElement.getBoundingClientRect();
 
+        let canvis = {
+            top: rect.top-rectComp.top,
+            left: rect.left-rectComp.left,
+            width: rect.width,
+            height: rect.height,
+        };
+        if (animacio) {
+            this.movent = true;
+            $(this.pill.nativeElement).animate(canvis, 400, () => { this.movent = false; });
+        }
+        else
+            $(this.pill.nativeElement).css(canvis);
+            
+    }
+
+    // Funcions //
+    @HostListener('window:resize', ['$event'])
+    @HostListener('window:scroll', ['$event'])
+    onResize() {
+        this.actPosPill(false);
+    }
 
     
 }
