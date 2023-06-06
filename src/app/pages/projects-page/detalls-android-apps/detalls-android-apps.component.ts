74cade0b615b031806d3db0b00e2d1a3b3f1b11e
Jordi Mas <xordimas@gmail.com>
2023-06-06 05:29:14 +0200

    Detalls Android apps

--- src/app/pages/projects-page/detalls-android-apps/detalls-android-apps.component.ts
+++ src/app/pages/projects-page/detalls-android-apps/detalls-android-apps.component.ts
@@ -1,14 +1,15 @@
+import * as $ from "jquery";
 import { Component } from '@angular/core';
 import { MainService } from 'src/app/services/main.service';
 
 @Component({
-  selector: 'app-detalls-android-apps',
-  templateUrl: './detalls-android-apps.component.html',
-  styleUrls: ['./detalls-android-apps.component.scss']
+    selector: 'app-detalls-android-apps',
+    templateUrl: './detalls-android-apps.component.html',
+    styleUrls: ['./detalls-android-apps.component.scss']
 })
 export class DetallsAndroidAppsComponent {
 
-  constructor(public m: MainService) { }
+    constructor(public m: MainService) { }
 
     ngOnInit() {
         this.m.afterRootFadeIn(this.afterRootFadeIn.bind(this));
@@ -18,4 +19,20 @@ export class DetallsAndroidAppsComponent {
     
     afterRootFadeIn() { }
   
+  
+    toggleSeccio(boto) {
+        let $seccio = $(boto).parent();
+        let $fletxa = $seccio.find(".fletxa");
+        let $contingut = $seccio.find(".contingut");
+        let obert = $fletxa.is("[data-open]");
+
+        obert = !obert;
+        if (obert) {
+            $fletxa.attr("data-open", "");
+            $contingut.stop().slideDown(200);
+        } else {
+            $fletxa.removeAttr("data-open");
+            $contingut.stop().slideUp(200);
+        }
+    }
 }
