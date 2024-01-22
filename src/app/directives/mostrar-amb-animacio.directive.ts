986e79b5623bf6a1a1084be7dc93f755ace1b68d
JORDIMAS-PC\xordi <xordimas@gmail.com>
2024-01-22 02:03:30 +0100

    Improvement of mostrarAmbAnimacioDirective in order to add a small, incremental delay, among all the sibling elements up to 2 levels from the affected element, without need of pass parameters or send any explicit information into the directive.

--- src/app/directives/mostrar-amb-animacio.directive.ts
+++ src/app/directives/mostrar-amb-animacio.directive.ts
@@ -21,19 +21,9 @@ export class MostrarAmbAnimacioDirective implements OnInit, OnDestroy {
         this.observer = new IntersectionObserver(entries => {
             entries.forEach(async entry => {
                 if (entry.isIntersecting) {
-                    // Retard animació //
-                    await Utils.wait(100 + Math.random() * 150);
-                    
-                    this.renderer.addClass(entry.target, 'animacio');
-                    
-                    this.renderer.addClass(entry.target, 'mostrat');
-                    
-                    this.timeoutTreureAnimacio = setTimeout(() => {
-                        this.renderer.removeClass(entry.target, 'animacio');
-                    }, 300);
+                    await this.mostrar(entry);
                 } else {
-                    this.renderer.removeClass(entry.target, 'mostrat');
-                    clearTimeout(this.timeoutTreureAnimacio);
+                    this.ocultar(entry);
                 }
             });
         });
@@ -42,8 +32,49 @@ export class MostrarAmbAnimacioDirective implements OnInit, OnDestroy {
     }
 
     ngOnDestroy() {
-        if (this.observer) {
+        if (this.observer)
             this.observer.disconnect();
+    }
+
+    async mostrar(entry) {
+        if (!entry.isVisible) {
+
+            // Recuperem delay de les dades de l'element prèviament guardades. Si no hi és, el calculem //
+            let delay = this.el["animationDelay"];
+            if (!delay) {
+                delay = 100 + this.getPositionAmongSiblings() * 30
+                this.el["animationDelay"] = delay;
+            }
+
+            await Utils.wait(delay);
+
+            
+            this.renderer.addClass(entry.target, 'transicions');
+            this.renderer.addClass(entry.target, 'mostrat');
+            
+            this.timeoutTreureAnimacio = setTimeout(() => {
+                this.renderer.removeClass(entry.target, 'transicions');
+            }, 300);
         }
+    }
+
+    ocultar(entry) {
+        clearTimeout(this.timeoutTreureAnimacio);
+        this.renderer.removeClass(entry.target, 'transicions');
+        this.renderer.removeClass(entry.target, 'mostrat');
+    }
+
+    getPositionAmongSiblings() {
+        // Aconseguir la posició de l'element entre els seus germans i cosins //
+        var currentElement = $(this.el.nativeElement);
+        var sameLevelElements = currentElement
+            .add('[appMostrarAmbAnimacio]')
+            .add(currentElement)
+            .filter(function (i, e) {
+                // Comptar pares fins al segon nivell, o sigui germans i cosins //
+                return $(e).parents()[2] == currentElement.parents()[2];
+            });
+        
+        return sameLevelElements.index(currentElement);
     }
 }
