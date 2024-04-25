b522a59d32fcbb620c8f8ca69c219240567df204
JORDIMAS-PC\xordi <xordimas@gmail.com>
2024-04-25 23:50:36 +0200

    Languages: Add sort button Languages: scroll when showing experience time

--- src/app/pages/home-page/languages/languages.component.ts
+++ src/app/pages/home-page/languages/languages.component.ts
@@ -11,6 +11,7 @@ import { Utils } from 'src/app/shared/utils';
 export class LanguagesComponent extends CardComponent {
 
     public showTime = false;
+    public sorted = false;
 
     constructor(
         public override m: MainService,
@@ -20,15 +21,40 @@ export class LanguagesComponent extends CardComponent {
 
         this.showTime = Utils.getCookie("showTime") == "1";
     }
-    override async ngOnInit() {
+    override ngOnInit() {
         super.ngOnInit();
     }
 
 
     showExpTime_click() {
         this.showTime = !this.showTime;
+        
+        // Desordenem //
+        if (!this.showTime) {
+            this.sort_click(false);
+        }
+
+        // Scroll to the element //
+        window.scrollTo({ top:
+            document.getElementById("languages")!.offsetTop
+            - (this.m.appbar.height() + (this.m.index.vistaMobil() ? this.m.index.index.nativeElement.offsetHeight : 10))
+        });
 
         Utils.setCookieDays("showTime", this.showTime ? 1 : 0);
+    }
+    sort_click(sorted?) {
+        if (sorted != undefined) this.sorted = sorted
+        else this.sorted = !this.sorted;
+
+        // Afegim o traiem l'estil order per a ordenar elements dins el flex //
+        if (this.sorted) {
+            $("app-skill").each((i, e) => {
+                let order = this.m.exp.getSkill(e.getAttribute("n")).diesTotals || 0;
+                e.style.order = -order + "";
+            });
+        } else {
+            $("app-skill").css("order", "");
+        }
     }
 
     obrirVeureMesSkills() {
