cddf0e41a9388a45713bf39cd1fa6a4e402f12ae
Jordi Mas Parramon <xordimas@gmail.com>
2025-02-07 01:07:00 +0100

    Change urls of android demo videos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     assets: "https://jordimas96.github.io/assets_repo/jordimas96.github.io/", // Amb GitHub Pages //
     assets_raw: "https://raw.githubusercontent.com/jordimas96/assets_repo/main/jordimas96.github.io/", // Arxiu raw //
+
+    assets_media_raw: "https://media.githubusercontent.com/media/jordimas96/assets_repo/main/jordimas96.github.io/", // Arxiu raw (LFS) //
 };
