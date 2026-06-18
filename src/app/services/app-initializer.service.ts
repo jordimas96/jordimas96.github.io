dc8aa8b8b97bec7565216a83aec1323afc0592e7
Jordi Mas Parramon <xordimas@gmail.com>
2026-06-18 12:39:24 +0200

    Fix splash screen getting stuck

--- src/app/services/app-initializer.service.ts
+++ src/app/services/app-initializer.service.ts
@@ -9,7 +9,7 @@ export class AppInitializerService {
     private readonly FONS_URL = "assets/background/boles.webp";
 
     constructor(private appRef: ApplicationRef) { }
-    
+
 
     async init() {
 
@@ -18,10 +18,13 @@ export class AppInitializerService {
         // Aplicar fons al body //
         backgroundImageLoaded.then(() => document.body.style.backgroundImage = `url("${this.FONS_URL}")`);
 
-        await Promise.all([
-            backgroundImageLoaded,
-            document.fonts.ready,
-            firstValueFrom(this.appRef.isStable.pipe(filter(stable => stable))),
+        await Promise.race([
+            Promise.all([
+                backgroundImageLoaded,
+                document.fonts.ready,
+                firstValueFrom(this.appRef.isStable.pipe(filter(stable => stable))),
+            ]),
+            new Promise(resolve => setTimeout(resolve, 3000)),
         ]);
 
         setTimeout(() => {
@@ -47,11 +50,15 @@ export class AppInitializerService {
     ocultarSplashScreen() {
         let splashScreen = document.getElementById("splash-screen");
         if (!splashScreen) return;
-        
+
         splashScreen.style.opacity = "0";
         splashScreen.style.pointerEvents = "none";
-        
-        splashScreen.addEventListener("transitionend", () => splashScreen.remove());
+
+        splashScreen.addEventListener("transitionend", () => splashScreen.remove(), { once: true });
+
+        setTimeout(() => {
+            splashScreen.remove();
+        }, 1000);
     }
 
 
